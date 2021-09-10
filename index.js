92c5d47ee795e35cf466041b3def6d1ae1778c22
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2021-09-10 16:34:29 -0300

    Uso da variável de ambiente "port" para definição do número da porta do serviço

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.port || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
